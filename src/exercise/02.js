e9a9efdd9ea58b5fd9a3e795f068c4cbbb9d780f
Duncan Scott <duncan_scott@outlook.com>
2022-01-31 12:28:54 +0000

    Exercise 02 complete

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,18 +3,52 @@
 
 import * as React from 'react'
 
-function Greeting({initialName = ''}) {
-  // 🐨 initialize the state to the value from localStorage
-  // 💰 window.localStorage.getItem('name') ?? initialName
-  const [name, setName] = React.useState(initialName)
+// Custom hook
+function useLocalStorageState(
+    key,
+    defaultValue = "",
+    // the = {} fixes the error we would get from destructuring when no argument was passed
+    // Check https://jacobparis.com/blog/destructure-arguments for a detailed explanation
+    { serialize = JSON.stringify, deserialize = JSON.parse } = {}) {
+  const [value, setValue] = React.useState(() => {
+    const localStorageValue = window.localStorage.getItem(key);
 
-  // 🐨 Here's where you'll use `React.useEffect`.
-  // The callback should set the `name` in localStorage.
-  // 💰 window.localStorage.setItem('name', name)
+    if (localStorageValue) {
+      // the try/catch is here in case the localStorage value was set before
+      // we had the serialization in place
+      try {
+        return deserialize(localStorageValue);
+      } catch (error) {
+        window.localStorage.removeItem(key);
+      }
+    }
+
+    return typeof defaultValue === "function" ? defaultValue() : defaultValue;
+  })
+
+  const previousKeyRef = React.useRef(key);
+
+  React.useEffect(() => {
+    const previousKey = previousKeyRef.current;
+
+    if (previousKey !== key) {
+      window.localStorage.removeItem(previousKey);
+    }
+
+    previousKeyRef.current = key;
+    window.localStorage.setItem(key, serialize(value));
+  }, [key, value, serialize]);
+
+  return [value, setValue];
+}
+
+function Greeting({initialName = ''}) {
+  const [name, setName] = useLocalStorageState("name", initialName);
 
   function handleChange(event) {
     setName(event.target.value)
   }
+
   return (
     <div>
       <form>
@@ -27,7 +61,7 @@ function Greeting({initialName = ''}) {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName="<Enter name>" />
 }
 
 export default App
